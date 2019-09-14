b0dae0c7c6cab562cf6c57c387559272a043159e
hassan.mubarka <hassan.mubarka@cs104u-4b.acad.fullerton.edu>
2019-09-14 13:53:01 -0700

    underflow and overflow visual

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -8,15 +8,15 @@ int main()
   // then print both the char and the short
 
   char my_char1 = 'J';
-  short my_short1 = ;
-  std::cout << "The ASCII value for " << my_char1 << " is " << my_short1 << std::endl;
+  short my_short1 = 74 ;
+  std::cout << "The ASCII value for " << static_cast<int>(my_char1) << " is " << my_short1 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
 
   // TODO#2: add one to the character to see what gets printed out
 
   char my_char2 = 'C';
-  my_char2 = ;
+  my_char2 = my_char2 + 1 ;
   std::cout << "Char1: " << my_char2 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
@@ -26,8 +26,8 @@ int main()
   // longer alphabetic
 
   char my_char3 = 'z';
-  my_char3 = ;
-  std::cout << "Char2: " << my_char3 << std::endl;
+  my_char3 = my_char3 + 1 ;
+  std::cout << "Char2: " << my_char3 << std::endl; // Curly bracket is shown
 
   std::cout << std::endl; // blank line to separate output from the different exercises
 
@@ -42,14 +42,16 @@ int main()
 
   std::cout << "Short (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_short2 << std::hex << " (0x" << my_short2 << ')';
-  my_short2 = ;
+  my_short2 = my_short2 + 1 ;
   std::cout << ", after adding 1: " << std::dec << my_short2
+  // max for short was exceeded so it goes to lowest value
     << std::hex << " (0x" << my_short2 << ")\n";
 
   std::cout << "Int (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_int2 << std::hex << " (0x" << my_int2 << ')';
-  my_int2 = ;
+  my_int2 = my_int2 + 1 ;
   std::cout << ", after adding 1: " << std::dec << my_int2
+  // int goes up to 65535 so it doesn't become negative when adding 1
     << std::hex << " (0x" << my_int2 << ")\n";
 
   std::cout << std::endl; // blank line to separate output from the different exercises
@@ -63,14 +65,16 @@ int main()
 
   std::cout << "Unsigned short (" << sizeof(my_short3) << " bytes): "
     << std::dec << my_short3 << std::hex << " (0x" << my_short3 << ')';
-  my_short3 = ;
+  my_short3 = my_short3 + 1 ;
   std::cout << ", after adding 1: " << std::dec << my_short3
+  // max for unsighned short was exceeded
     << std::hex << " (0x" << my_short3 << ")\n";
 
   std::cout << "Same value as int (" << sizeof(my_int3) << " bytes): "
     << std::dec << my_int3 << std::hex << " (0x" << my_int3 << ')';
-  my_int3 = ;
+  my_int3 = my_int3 + 1 ;
   std::cout << ", after adding 1: " << std::dec << my_int3
+  // was not exceeded
     << std::hex << " (0x" << my_int3 << ")\n";
 
   // ** not required, but for more practice, feel free to add code below which **
