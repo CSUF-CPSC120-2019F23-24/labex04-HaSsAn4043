531a9322438777b8057d9068d814e40c1efe6a43
hassan.mubarka <hassan.mubarka@cs104u-4b.acad.fullerton.edu>
2019-09-14 14:39:17 -0700

    2019 prediction calculator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,15 @@
 // Sales prediction
+#include <iostream>
+
+int main()
+{
+  const double PERCENT_INCREASE = 1.18;
+  const int _2018_SALES = 2103419277;
+
+  long sales_for_2019 = _2018_SALES * PERCENT_INCREASE;
+
+  std::cout << "Last year's sales were $" << _2018_SALES << std::endl;
+  std::cout << "This year's sales prediction: $ " << sales_for_2019 << std::endl;
+
+  return 0;
+}
